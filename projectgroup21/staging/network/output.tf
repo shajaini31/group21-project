1fe84ee3f108c8ed4b99edf258342c84dddf48ee
shajaini <ssasikumar-vijaya-ni@myseneca.ca>
2022-04-16 19:44:59 +0000

    Stagging Newtowk changes

---
+++ projectgroup21/staging/network/output.tf
@@ -0,0 +1,10 @@
+output "public_subnet_ids" {
+  value = module.vpc-staging.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  value = module.vpc-staging.private_subnet_ids
+}
+output "vpc_id" {
+  value = module.vpc-staging.vpc_id
+}
