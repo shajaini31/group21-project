eeb904d3d1a869c90ae3d48ef7cb1024842cbafb
Ranjan Gnanasekaran <rgnanasekaran@myseneca.ca>
2022-04-16 05:07:31 +0000

    commiting my dev code as first commit

---
+++ projectgroup21/dev/webservers/variables.tf
@@ -0,0 +1,44 @@
+# Instance type
+variable "instance_type" {
+  default = {
+    "prod"    = "t3.medium"
+    "staging" = "t3.small"
+    "dev"     = "t3.micro"
+  }
+  description = "Type of the instance"
+  type        = map(string)
+}
+
+# Default tags
+variable "default_tags" {
+  default = {
+    "Owner" = "group21"
+    "App"   = "Web"
+  }
+  type        = map(any)
+  description = "Default tags to be appliad to all AWS resources"
+}
+
+# Prefix to identify resources
+variable "prefix" {
+  default     = "group21"
+  type        = string
+  description = "Name prefix"
+}
+
+
+# Variable to signal the current environment 
+variable "env" {
+  default     = "dev"
+  type        = string
+  description = "Deployment Environment"
+}
+
+variable "num_linux_vms" {
+  default     = 2
+  description = "Number of Linux VMs to provision"
+  type        = number
+}
+
+
+
