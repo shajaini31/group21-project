39737aec29235566bb5055ed68bce3181ad23dfb
shajaini <ssasikumar-vijaya-ni@myseneca.ca>
2022-04-16 18:30:10 +0000

    new changes

--- projectgroup21/dev/webservers/config.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "dev-acsproject-group21"           // Bucket where to SAVE Terraform State
-    key    = "dev-webservers/terraform.tfstate" // Object name in the bucket to SAVE Terraform State
-    region = "us-east-1"                        // Region where bucket is created
-  }
-}
--- projectgroup21/staging/network/config.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "staging-acsproject-group21"   // Bucket where to SAVE Terraform State
-    key    = "staging-network/terraform.tfstate" // Object name in the bucket to SAVE Terraform State
-    region = "us-east-1"                     // Region where bucket is created
-  }
-}
